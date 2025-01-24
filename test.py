8e88a438f750798ab529bc6e156a4c2532262ca0
李沉舟 <ax909@163.com>
2025-01-24 14:23:40 +0800

    test to map

--- test.py
+++ test.py
@@ -1,1 +1,42 @@
-import transformers
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def __init__(self, root_value):
+        self.root = TreeNode(root_value)
+
+    def insert_left(self, current_node, value):
+        if current_node.left is None:
+            current_node.left = TreeNode(value)
+        else:
+            new_node = TreeNode(value)
+            new_node.left = current_node.left
+            current_node.left = new_node
+
+    def insert_right(self, current_node, value):
+        if current_node.right is None:
+            current_node.right = TreeNode(value)
+        else:
+            new_node = TreeNode(value)
+            new_node.right = current_node.right
+            current_node.right = new_node
+
+    def inorder_traversal(self, node):
+        if node:
+            self.inorder_traversal(node.left)
+            print(node.value)
+            self.inorder_traversal(node.right)
+
+
+# 示例用法
+if __name__ == "__main__":
+    tree = BinaryTree(10)
+    tree.insert_left(tree.root, 5)
+    tree.insert_right(tree.root, 15)
+    tree.insert_left(tree.root.left, 2)
+    tree.insert_right(tree.root.left, 7)
+    tree.inorder_traversal(tree.root)
